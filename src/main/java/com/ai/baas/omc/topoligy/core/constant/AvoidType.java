39c75e531bca2672e7d806063feb90fff96d808a
JackieLiu <liutong6053@163.com>
2016-04-25 11:34:53 +0800

    添加正确文件名文件.

---
+++ src/main/java/com/ai/baas/omc/topoligy/core/constant/AvoidType.java
@@ -0,0 +1,26 @@
+package com.ai.baas.omc.topoligy.core.constant;
+
+/**
+ * 免催类型
+ */
+public final class AvoidType {
+	/**
+	 * AVOID_STOP 免停
+	 */
+	public static final String AVOID_STOP = "STOP";
+
+	/**
+	 * AVOID_URGE 免催
+	 */
+	public static final String AVOID_URGE = "URGE";
+
+	/**
+	 * AVOID_STOPANDURGE 免停催
+	 */
+	public static final String AVOID_STOPANDURGE = "STOPANDURGE";
+
+	/**
+	 * AVOID_ALL 红名单
+	 */
+	public static final String AVOID_ALL = "REDLIST";
+}
